bccff4425374f5ed726ac25cce130e427fb6a3d5
Rabkaps <102897583+Rabkaps@users.noreply.github.com>
2026-06-14 13:07:44 +0330

    chore: bump version to 1.0.59

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hambalapps.expressivebox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 58
-        versionName = "1.0.58"
+        versionCode = 59
+        versionName = "1.0.59"
         ndk {
             abiFilters += "arm64-v8a"
         }
